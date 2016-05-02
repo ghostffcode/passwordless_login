d55b17cb0b9ed2006a1ec999c7092bb3641dbab7
PoeticBliss <meremezeukah@gmail.com>
2016-05-02 13:10:02 -0400

    Started the passwordless project

---
+++ passwordless.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * This is a simple user authentication system without password
+ * It only works and asks the user for the code sent to their email address to work
+ */
+class passwordless {
+
+  function __construct ($email = '') {
+    # code...
+  }
+
+  public function isLoggedIn () {
+    // This function will check and see if the user is logged in
+    # code...
+  }
+
+
+}
+ ?>
