77e76d08ad73c235f74352eae9c21b1b824d01e5
PoeticBliss <meremezeukah@gmail.com>
2016-05-02 23:15:53 -0400

    Updated functionalities

--- inc/passwordless.php
+++ inc/passwordless.php
@@ -7,6 +7,7 @@
 class passwordless {
 
   private $format = 'json';
+  private $trial = 3;
 
   function __construct($f = '') {
     // Start a user session on class instance
@@ -32,10 +33,14 @@ class passwordless {
         // Send the code as email to the user
         if ($this->sendEmail($email, $code)) {
           $_SESSION['code'] = $code;
+          $res['code'] = $_SESSION['code'];
           $_SESSION['email'] = $email;
+          // Set a trial counter in the session
+          $_SESSION['trial'] = 0;
           $res['emailSuccess'] = true;
         } else {
           $res['emailSuccess'] = false;
+          $res['code'] = $code;
         }
       } else {
         // Let user know that the email is invalid
@@ -82,26 +87,90 @@ class passwordless {
   }
 
   private function sendEmail ($email = '', $code = '') {
+    $res = false;
     // This will handle the validation code email sending
+    $msg = 'Your code is'. $code;
+    $subject = 'Passwordless Login code';
 
-    return true;
+    // Check if the email was a success and return true, else return false
+    if (mail($email,$subject,$msg)) {
+      $res = true;
+    }
+    return $res;
   }
 
   public function validate ($code = '') {
+    $res = ['info'=> '', 'loggedin' => false, 'detail'=> null];
     // This function will validate the code emailed to the user
-    # code...
+
+    // Check to be sure is not logged in already
+    if (!isset($_SESSION['loggedin'])) {
+      // Check if the input is a number and session is set
+      if (is_numeric($code) && isset($_SESSION)) {
+        // Check if code matches the code in session
+        if ($code === $_SESSION['code']) {
+          // if true, log the user in by setting up necessary session values
+          $_SESSION['loggedin'] = 1;
+          // Remove code from session or not, whatever
+          unset($_SESSION['code']);
+          // Remove trial counter from session
+          unset($_SESSION['trial']);
+          // Setup the return array variable
+          $res['info'] = 'success';
+          $res['loggedin'] = true;
+        }
+      } else {
+        // Each time it fails, increment the trial times by 1
+        if (isset($_SESSION['trial'])) { $_SESSION['trial'] += 1; }
+        // Also check if the limit has been reached
+        if ($_SESSION['trial'] >= $this->trial) {
+          // If limit has been reached, resend an email
+          $res['newdetail'] = json_decode($this->sendCode($_SESSION['email']));
+          // Setup the return array variable
+          $res['info'] = 'error';
+          $res['loggedin'] = false;
+          $res['detail'] = 'You have reached your limit. New code sent to Email';
+        } else {
+          $res['info'] = 'error';
+          $res['loggedin'] = false;
+          $res['detail'] = 'Code you entered is invalid';
+        }
+      }
+    } else {
+      $res['info'] = 'error';
+      $res['loggedin'] = true;
+      $res['detail'] = 'You\'ve already been logged in';
+    }
+
+    if ($this->format == 'array') {
+      return $res;
+    } else {
+      return json_encode($res);
+    }
   }
 
   public function isLoggedIn () {
     // This function will check and see if the user is logged in
-    # code...
+    $res = false;
+    // Check if the session isset
+    if (isset($_SESSION)) {
+      // If true, check if the loggedin session variable is one of them
+      if (isset($_SESSION['loggedin']) && ($_SESSION['loggedin'] === 1)) {
+        $res = true;
+      }
+    }
+    return $res;
   }
 
   public function logOut () {
     // This will log the user out
-    # code...
-  }
+    // remove all session variables
+    session_unset();
+    // destroy the session
+    session_destroy();
 
+    return true;
+  }
 
 }
  ?>
