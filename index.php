dfcd8da580b8799d8a3939b46353f36df48cd16b
PoeticBliss <meremezeukah@gmail.com>
2016-05-03 00:10:46 -0400

    Added url get as implementation system

--- index.php
+++ index.php
@@ -1,4 +1,17 @@
 <?php
 // This is the sample file to run it
 
+include 'inc/passwordless.php';
+
+$p = new passwordless('json');
+
+if (isset($_GET['code'])) {
+  echo $p->validate($_GET['code']);
+} else if (isset($_GET['email'])) {
+  echo $p->sendCode($_GET['email']);
+} else {
+echo 'Submit value (Email or code) using php url get for trial';
+}
+
+//unset($_SESSION['loggedin']);
 ?>
